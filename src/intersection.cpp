d8c7cd07b70ba7ac529550bf2254a61d1ebf29eb
petras <petrasvestartas@gmail.com>
2024-10-23 19:15:29 +0200

    ADD: start of the line_plane intersection

--- src/intersection.cpp
+++ src/intersection.cpp
@@ -6,9 +6,64 @@ namespace geo {
         // Constructor implementation
     }
 
-    // Static dummy method implementation
-    void Intersection::dummyMethod() {
-        log("Dummy method called.");
+    void Intersection::line_plane(const Line &line, const Plane &plane, Point &output, bool is_finite) {
+
+        // 
+        // bool rc = false;
+        // double a, b, d, fd, t;
+
+        // auto pt0 = line[0];
+        // auto pt1 = line[1];
+        // a = internal::value_at(plane, pt0);
+        // b = internal::value_at(plane, pt1);
+        // d = a - b;
+        // if (d == 0.0)
+        // {
+        //     if (fabs(a) < fabs(b))
+        //         t = 0.0;
+        //     else if (fabs(b) < fabs(a))
+        //         t = 1.0;
+        //     else
+        //         t = 0.5;
+        // }
+        // else
+        // {
+        //     d = 1.0 / d;
+        //     fd = fabs(d);
+        //     if (fd > 1.0 && (fabs(a) >= ON_DBL_MAX / fd || fabs(b) >= ON_DBL_MAX / fd))
+        //     {
+        //         // double overflow - line may be (nearly) parallel to plane
+        //         t = 0.5;
+        //     }
+        //     else
+        //     {
+        //         t = a / (a - b); // = a*d;  a/(a-b) has more precision than a*d
+        //         rc = true;
+        //     }
+        // }
+
+        // // if (line_parameter)
+        // //     *line_parameter = t;
+
+        // // s[0].z()
+        // //  26 Feb 2003 Dale Lear
+        // //      Changed
+        // //           return (1-t)*from + t*to;
+        // //      to the following so that axis aligned lines will
+        // //      return exact answers for large values of t.
+        // //      See RR 9683.
+
+        // const double s = 1.0 - t;
+
+        // output = IK::Point_3(
+        //     (line[0].x() == line[1].x()) ? line[0].x() : s * line[0].x() + t * line[1].x(),
+        //     (line[0].y() == line[1].y()) ? line[0].y() : s * line[0].y() + t * line[1].y(),
+        //     (line[0].z() == line[1].z()) ? line[0].z() : s * line[0].z() + t * line[1].z());
+
+        // if (is_finite && (t < 0 || t > 1))
+        //     return false;
+
+        // return rc;
     }
 
     // Add method implementations here
--- src/intersection.hpp
+++ src/intersection.hpp
@@ -8,8 +8,17 @@ namespace geo {
         // Constructor
         Intersection();
 
-        // Static dummy method
-        static void dummyMethod();
+        /**
+         * Get the intersection of a line and a plane
+         * https://github.com/petrasvestartas/wood/blob/main/cmake/src/wood/include/cgal_intersection_util.cpp
+         *
+         * @param [in] line segment
+         * @param [in] plane plane
+         * @param [out] output the point output
+         * @param [out] is_finite true if the line is finite, still the point will be outputed
+         * @return true the intersection is sucsessful or point is outside the line, incase the finite search is used
+         */
+        static void line_plane(const Line &line, const Plane &plane, Point &output, bool is_finite);
 
     private:
         // Add private methods and members here
