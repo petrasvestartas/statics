68e4653ca058d2bb52a7feec410dce288ef5db44
petras <petrasvestartas@gmail.com>
2024-10-23 19:11:15 +0200

    ADD: Intersection class

---
+++ src/intersection.cpp
@@ -0,0 +1,15 @@
+#include "intersection.hpp"
+
+namespace geo {
+    // Constructor
+    Intersection::Intersection() {
+        // Constructor implementation
+    }
+
+    // Static dummy method implementation
+    void Intersection::dummyMethod() {
+        log("Dummy method called.");
+    }
+
+    // Add method implementations here
+}
---
+++ src/intersection.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "core.hpp"
+
+namespace geo {
+    class Intersection {
+    public:
+        // Constructor
+        Intersection();
+
+        // Static dummy method
+        static void dummyMethod();
+
+    private:
+        // Add private methods and members here
+    };
+}
